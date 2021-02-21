f7a70ba3dd37e246083f03fac50bf7e41bfe7db3
Dufour-Couture, Alex <dufour-couture.alex@courrier.uqam.ca>
2021-02-21 11:05:56 -0500

    add captcha

--- assets/js/sendapi.js
+++ assets/js/sendapi.js
@@ -1,59 +1,28 @@
-function submitToAPI(e) {
-    e.preventDefault();
-    var URL = "https://6urxe2yiti.execute-api.ca-central-1.amazonaws.com/Production/contact-us";
+$(document).ready(function() {
 
-         var Namere = /[A-Za-z]{1}[A-Za-z]/;
-         if (!Namere.test($("#name-input").val())) {
-                $("#name-error").css({ visibility: "visible",display: "block"});
-                // $(".name-error").addClass("msg-error");
-                    //  alert ("Name can not less than 2 char");
-             return;
-         }
-         var mobilere = /[0-9]{10}/;
-         if (!mobilere.test($("#phone-input").val())) {
-             alert ("Please enter valid mobile number");
-             return;
-         }
-         if ($("#email-input").val()=="") {
-             alert ("Please enter your email id");
-             return;
-         }
+  $("#submit").click(function(e) {
+      e.preventDefault();
 
-         var reeamil = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,6})?$/;
-         if (!reeamil.test($("#email-input").val())) {
-             alert ("Please enter valid email address");
-             return;
-         }
+      var name = $("#name").val(),
+          email = $("#email").val(),
+          comment = $("#comment").val();
 
-    var name = $("#name-input").val();
-    var phone = $("#phone-input").val();
-    var email = $("#email-input").val();
-    var desc = $("#description-input").val();
-    var data = {
-       name : name,
-       phone : phone,
-       email : email,
-       desc : desc
-     };
-
-    $.ajax({
-      type: "POST",
-      url : " https://6urxe2yiti.execute-api.ca-central-1.amazonaws.com/Production/reCaptcha",
-      dataType: "json",
-      crossDomain: "true",
-      contentType: "application/json; charset=utf-8",
-      data: JSON.stringify(data),
-
-      
-      success: function () {
-        // clear form and show a success message
-        alert("Successfull");
-        document.getElementById("contact-form").reset();
-    location.reload();
-      },
-      error: function () {
-        // show an error message
-        alert("UnSuccessfull");
-      }});
-  };
+      $.ajax({
+          type: "POST",
+          url: 'https://6urxe2yiti.execute-api.ca-central-1.amazonaws.com/Production/reCaptcha',
+          contentType: 'application/json',
+          data: JSON.stringify({
+              'name': name,
+              'email': email,
+              'comment': comment
+          }),
+          success: function(res){
+              $('#contact-submit').text('Email was sent.');
+          },
+          error: function(){
+              $('#contact-submit').text('Error.');
+          }
+      });
+  })
+});
 
